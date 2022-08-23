1c12fe786c871500086e7145b6506c4773d8045e
Vitor <vhpsantos@gmail.com>
2022-08-23 19:42:06 -0300

    Implementando API no meu-portifolio

--- APIgithub.js
+++ APIgithub.js
@@ -9,9 +9,74 @@ let pegar = await axios.get(url).then(d => {
             arr.push(index) // Empurrar todos os obj para o array da linha 2 
         }
     })
+    for (let i = 0; i < 4; i++){
+        createElem(i)
+    }
 }
 )
 
 }
 infoAPI()
 
+// Criação de elementos
+
+function createElem(num){
+    // Box-Projeto
+    let projectContainer = document.querySelector('.projetos-container')
+    let projeto = document.createElement('div')
+    projeto.classList.add('projeto')
+    projectContainer.appendChild(projeto)
+    // header-box
+    let headerbox = document.createElement('div')
+    headerbox.classList.add('header-box')
+    projeto.appendChild(headerbox)
+    
+    let imgHB = document.createElement('img')
+    imgHB.src = "./assets/folder.svg"
+    headerbox.appendChild(imgHB)
+
+    let pname = document.createElement('p')
+    pname.classList.add('project-name')
+    headerbox.appendChild(pname)
+    pname.innerHTML = `${arr[num].name}`
+
+    let pdescription = document.createElement('p')
+    pdescription.classList.add('project-description')
+    projeto.appendChild(pdescription)
+    pdescription.innerHTML = `${arr[num].description}`
+    // footer-box
+    let footerbox = document.createElement('div')
+    footerbox.classList.add('footer-box')
+
+    //Stars
+    projeto.appendChild(footerbox)
+    let imgStar = document.createElement('img')
+    imgStar.src = "./assets/star.svg"
+    footerbox.appendChild(imgStar)
+
+    let pStars = document.createElement('p')
+    pStars.classList.add('stars')
+    pStars.innerHTML = `${arr[num].stargazers_count}`
+    footerbox.appendChild(pStars)
+
+    //Branchs
+    let imgBranchs = document.createElement('img')
+    imgBranchs.src = "./assets/git-branch.svg"
+    footerbox.appendChild(imgBranchs)
+
+    let pBranch = document.createElement('p')
+    pBranch.classList.add('branchs')
+    pBranch.innerHTML = `10`
+    footerbox.appendChild(pBranch)
+    
+    // program-language
+    let divPL = document.createElement('div')
+    footerbox.appendChild(divPL)
+
+    let pPL = document.createElement('p')
+    pPL.classList.add('program-language')
+    pPL.innerHTML = `${arr[num].language}`
+    divPL.appendChild(pPL)
+  
+
+}
